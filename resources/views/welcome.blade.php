d6ee74d85132d337539e1cb0715ec705db3fd42d
Vince4130 <vinceblanca@gmail.com>
2024-06-24 15:27:06 +0200

    New welcome blade

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+<x-guest-layout>
+    <h3 class="title">Welcome in Food App</h3>
+    <div class="link">
+        <a href="{{ route('dashboard') }}">Log In</a>
+        <a href="{{ route('register') }}">Register</a>
+    </div>
+</x-guest-layout>
