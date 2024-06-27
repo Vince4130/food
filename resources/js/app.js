480659059716d75d931501506fbcc17dc3edca0f
Vince4130 <vinceblanca@gmail.com>
2024-06-27 13:54:54 +0200

    Css Sass vite config and lang configuration

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,11 @@
 import './bootstrap';
 
+// Added: Actual Bootstrap JavaScript dependency
+import 'bootstrap';
+
+// Added: Popper.js dependency for popover support in Bootstrap
+import '@popperjs/core';
+
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
