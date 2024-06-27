b74f030de97b269ae2a320c05806958d5dd0cc63
Vince4130 <vinceblanca@gmail.com>
2024-06-27 17:48:06 +0200

    Add routes and worde in lang fr.json

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\MeasurementController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +16,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    Route::prefix('/measurements')->group(function () {
+        Route::get('/index', [MeasurementController::class, 'index'])->name('measurements.index');
+        Route::get('/show', [MeasurementController::class, 'index'])->name('measurements.show');
+    });
+    
 });
 
 require __DIR__.'/auth.php';
