d2ee90828296d4227bf916e0fcda9058f1fe69d3
Vince4130 <vinceblanca@gmail.com>
2024-06-27 17:43:09 +0200

    Create measurements controller CRUD

---
+++ app/Http/Controllers/MeasurementController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Measurement;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class MeasurementController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $user = $request->user();
+
+        $mesures = DB::table('measurements')
+            ->select('date', 'weight', 'height')
+            ->join('users', 'users.id', '=', 'measurements.user_id')
+            ->where('users.id', $user->id)
+            ->paginate(10);
+
+        return view('measurements.index', ['user' => $user, 'mesures' => $mesures]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Measurement $measurement)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Measurement $measurement)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Measurement $measurement)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Measurement $measurement)
+    {
+        //
+    }
+}
