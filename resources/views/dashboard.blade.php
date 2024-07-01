6334506fe3dcdfcba1e1b19974562894c8354d70
Vince4130 <vinceblanca@gmail.com>
2024-07-01 18:28:03 +0200

    Add chartJS line and legends

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,25 +9,64 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("Votre dernière mesure d'IMC enregistrée le ") }} {{ date('d/m/Y', strtotime($mesure->date)) }}
+                    {{ __("Vos dernières mesure d'IMC et poids enregistrés le ") }} {{ date('d/m/Y', strtotime($mesure->date)) }}
                 </div>
                 <div class="dashboard__data">
-                    <div class="imc__donut">
+                    <!-- <div class="imc__donut">
                         <div id="donut"></div>
-                    </div>
+                    </div> -->
                     <input type="hidden" name="myImc" id="myImc" value="{{ $imc }}">
+                    <input type="hidden" name="myWeight" id="myWeight" value="{{ $mesure->weight }}">
+                    <input type="hidden" name="range" id="range" value="{{ json_encode($weightsRange) }}">
+
                     <div class="imc__donut">
                         <div id="imc" class="imc__donut--justgage">{{ $indicator }}</div>
+                        <div id="weight" class="imc__donut--justgage">Poids actuel</div>
                     </div>
-                   
-                    <div>
+
+                    <div class="imc__legend">
+                            <div class="imc__legend--cat imc__legend--maigre">
+                                <div title="[imc<18.5]">Maigreur</div>
+                            </div>
+                            <div class="imc__legend--cat imc__legend--normale">
+                                <div title="[18.5<imc<25]">Corpulence normale</div>
+                            </div>
+                            <div class="imc__legend--cat imc__legend--surpoids">
+                                <div title="[25<imc<30]">Surpoids</div>
+                            </div>
+                            <div class="imc__legend--cat imc__legend--obesitemod">
+                                <div title="[30<imc<35]">Obésité modérée</div>
+                            </div>
+                            <div class="imc__legend--cat imc__legend--obesitesev">
+                                <div title="[35<imc<40]">Obésisté sévère</div>
+                            </div>
+                            <div class="imc__legend--cat imc__legend--obesitemor">
+                                <div title="[imc>40]">Obésité morbide</div>
+                            </div>
+                    </div>
+                    <!-- <div>
                         <canvas id="imcChart"></canvas>
-                    </div>
+                    </div> -->
                 </div>
             </div>
         </div>
     </div>
 
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __("Courbe de poids du mois de ") }} {{ $currentMonth }}
+                </div>
+                <div class="dashboard__data">
+                    <input type="hidden" name="weights" id="weights" value="{{ json_encode($weights) }}">   
+                    <div>
+                        <canvas id="weightsChart"></canvas>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <script>
         // var myColors = [
         //     "#a3f5ff",
@@ -38,9 +77,16 @@
         //     "#f44242"
         // ];
 
-        var myImc = document.querySelector("#myImc").value;
+        var myImc    = document.querySelector("#myImc").value;
+        var myWeight = document.querySelector("#myWeight").value;
+        var range =  document.querySelector("#range").value;
+        range = JSON.parse(range);
 
-        var g = new JustGage({
+        for(i=0; i < range.length; i++) {
+            range[i] = Math.floor(range[i]*100)/100;
+        }
+
+        var g1 = new JustGage({
             id: "imc",
             value: myImc,
             min: 0,
@@ -88,34 +134,102 @@
             },
             gaugeWidthScale: 0.6,
             counter: true,
-            relativeGaugeSize: true            
+            relativeGaugeSize: true
+        });
+
+        var g2 = new JustGage({
+            id: "weight",
+            value: myWeight,
+            min: 0,
+            max: 300,
+            donut: true,
+            decimals: 2,
+            title: "Poids",
+            // levelColors: myColors,
+            customSectors: {
+                ranges: [{
+                    color: "#a3f5ff",
+                    lo: 0,
+                    hi: range[0] - 0.01
+                }, {
+                    color: "#54ce54",
+                    lo: range[0],
+                    hi: range[1] - 0.01
+                }, {
+                    color: "#ffff00",
+                    lo: range[1],
+                    hi: range[2] - 0.01
+                }, {
+                    color: "#ffb800",
+                    lo: range[2],
+                    hi: range[3] - 0.01
+                }, {
+                    color: "#ff6800",
+                    lo: range[3],
+                    hi: range[4] - 0.01
+                }
+                , {
+                    color: "#f44242",
+                    lo: range[4],
+                    hi: 300
+                }]
+            },
+            pointer: true,
+            pointerOptions: {
+                toplength: -15,
+                bottomlength: 10,
+                bottomwidth: 12,
+                color: '#8e8e93',
+                stroke: '#ffffff',
+                stroke_width: 3,
+                stroke_linecap: 'round'
+            },
+            gaugeWidthScale: 0.6,
+            counter: true,
+            relativeGaugeSize: true
         });
     </script>
     <script>
-        const ctx = document.getElementById('imcChart');
+        const ctx     = document.getElementById('weightsChart');
+        var weights = document.querySelector("#weights").value;
+        weights = JSON.parse(weights);
 
+        for(i=0; i < weights.length; i++) {
+            weights[i] = Math.floor(weights[i]*100)/100;
+        }
+        console.log(weights);
         new Chart(ctx, {
-            type: 'doughnut',
+            type: 'line',
             data: {
-                labels: ['Obésité Modérée', 'Obésité Sévère', 'Obésité Morbide', 'Maigre', 'Normal', 'Surpoids'],
+                // labels: ['Obésité Modérée', 'Obésité Sévère', 'Obésité Morbide', 'Maigre', 'Normal', 'Surpoids'],
                 datasets: [{
-                    // label: '# of Votes',
-                    data: [35, 40, 41, 18.5, 25, 30],
-                    backgroundColor: [
-                        '#ffb800',
-                        '#ff6800',
-                        '#f44242',
-                        '#a3f5ff',
-                        '#54ce54',
-                        '#ffff00',
-                    ],
+                    label: 'Poids en Kg',
+                    data: weights,
                     borderWidth: 1
                 }]
             },
+            options: {
+                scales: {
+                    // x: {
+                    //     type: 'time',
+                    //     time: {
+                    //         unit: 'day'
+                    //     }
+                    // },
+                    y: {
+                        label: 'Poids',
+                        min: 0,
+                        max: 150
+                    }
+                }
+            }
             // options: {
             //     scales: {
-            //         y: {
-            //             beginAtZero: true
+            //         x: {
+            //             type: 'time',
+            //             time: {
+            //                 unit: 'month'
+            //             }
             //         }
             //     }
             // }
