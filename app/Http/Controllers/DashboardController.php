acf6d1c0cd4a06e8ce6e1d15fa09ebf809063a1d
Vince4130 <vinceblanca@gmail.com>
2024-07-01 18:25:09 +0200

    Add functions in dashboard controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,6 +7,7 @@ use App\Models\Measurement;
 use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Ramsey\Uuid\Type\Decimal;
+use Carbon\Carbon;
 
 class DashboardController extends Controller
 {
@@ -14,26 +15,66 @@ class DashboardController extends Controller
     {
         $user = $request->user();
 
+        $firstDay = Carbon::now()->format('Y-m-01'); 
+        $lastDay  = Carbon::createFromFormat('Y-m-d', $firstDay)->endOfMonth()->format('Y-m-d');
+        $currentMonth = $this->frenchMonth($firstDay);
+
         $mesure = DB::table('measurements')
             ->select('measurements.id', 'date', 'weight', 'height')
             ->join('users', 'users.id', '=', 'measurements.user_id')
             ->where('users.id', $user->id)
+            ->orderByDesc('date')
             ->first();
-        
+
+        $weights = DB::table('measurements')
+        ->select('weight')
+        ->join('users', 'users.id', '=', 'measurements.user_id')
+        ->where('users.id', $user->id)
+        ->where('date', '>=', $firstDay)
+        ->where('date', '<=', $lastDay)
+        ->orderBy('date')
+        ->get();
+    
         $imc = $this->calculateImc($mesure);
 
         $indicator = $this->imcIndicator($imc);
 
-        return view('dashboard', ['imc' => $imc, 'indicator' => $indicator, 'mesure' => $mesure]);   
+        $weightsRange   = $this->weightIndicator($mesure->height);
+
+        return view('dashboard', [
+            'imc' => $imc,
+            'indicator' => $indicator,
+            'mesure' => $mesure,
+            'weights' => $weights,
+            'weightsRange' => $weightsRange,
+            'currentMonth' => $currentMonth
+        ]);   
     }
 
-
+    
+    /**
+     * calculateImc
+     * Calcul l'imc en fonction de la 
+     * taille et du poids
+     * 
+     * @param  mixed $mesure
+     * @return float
+     */
     public function calculateImc($mesure): float
     {
         $imc = $mesure->weight/($mesure->height*$mesure->height)*10000;
+        
         return $imc;
     }
-
+    
+    /**
+     * imcIndicator
+     * Retourne le libellé correspondant 
+     * à l'imc 
+     * 
+     * @param  mixed $imc
+     * @return string
+     */
     public function imcIndicator(float $imc) : string
 
     {
@@ -67,4 +108,42 @@ class DashboardController extends Controller
         return $indicator;
     }
         
+    
+    /**
+     * weightIndicator
+     * Retourne un tableau contenant les fourchettes
+     * de poids en fonction de celles des imc
+     * 
+     * @param  mixed $height
+     * @return array
+     */
+    public function weightIndicator(int $height): array
+    {
+        $weights = [];
+
+        $imcRange = [18.5, 25, 30, 35, 40];
+
+        for($i=0; $i < count($imcRange); $i++) {
+            
+            $weights [] = ($imcRange[$i]*($height*$height))/10000;
+        }
+
+        return $weights;
+    }
+
+    
+    /**
+     * frenchMonth
+     * Retourne le mois courant en français
+     *
+     * @return string
+     */
+    public function frenchMonth() : string 
+    {
+        $months = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
+
+        $mois = Carbon::now()->format('n');
+
+        return $months[$mois-1];
+    } 
 }
