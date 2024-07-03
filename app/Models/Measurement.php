a9311927fe909c89ea2522546fabc2428ea20045
Vince4130 <vinceblanca@gmail.com>
2024-07-03 11:23:41 +0200

    Move db functions from controllers to models

--- app/Models/Measurement.php
+++ app/Models/Measurement.php
@@ -4,11 +4,54 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
+use PhpParser\Node\Expr\Cast\Object_;
 
 class Measurement extends Model
 {
     use HasFactory;
+    
+    /**
+     * getUserLastMesure
+     *
+     * @param  mixed $user
+     * @return Object
+     */
+    public static function getUserLastMesure(User $user): Object
+    {
+        $mesure = DB::table('measurements')
+            ->select('measurements.id', 'date', 'weight', 'height', 'sexe')
+            ->join('users', 'users.id', '=', 'measurements.user_id')
+            ->where('users.id', $user->id)
+            ->orderByDesc('date')
+            ->first();
 
+        return $mesure;
+    }
+    
+    /**
+     * getUserWeigthsOfCurrentMonth
+     *
+     * @param  mixed $user
+     * @param  mixed $firstDay
+     * @param  mixed $lastDay
+     * @return void
+     */
+    public static function getUserMesuresOfCurrentMonth(User $user, string $firstDay, string $lastDay)
+    {
+        $weights = DB::table('measurements')
+            ->select('date', 'weight', 'height')
+            ->join('users', 'users.id', '=', 'measurements.user_id')
+            ->where('users.id', $user->id)
+            ->where('date', '>=', $firstDay)
+            ->where('date', '<=', $lastDay)
+            ->orderBy('date')
+            ->get();
+
+        return $weights;
+    }
+    
      /**
      * The attributes that are mass assignable.
      *
--- app/Models/Morphology.php
+++ app/Models/Morphology.php
@@ -4,11 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
 
 class Morphology extends Model
 {
     use HasFactory;
 
+
+    public static function getUserMorphology(User $user): string
+    {
+        $morpho = DB::table('morphologies')
+            ->select('morpho')
+            ->join('users', 'users.id', '=', 'morphologies.user_id')
+            ->where('users.id', $user->id)
+            ->first();
+        
+        return $morpho->morpho;
+    }
+    
     /**
      * The attributes that are mass assignable.
      *
