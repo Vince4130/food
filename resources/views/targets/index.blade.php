200d428915b487887cf816af23f0aa03fff7a3df
Vince4130 <vinceblanca@gmail.com>
2024-07-12 17:25:38 +0200

    target index blade

--- resources/views/targets/index.blade.php
+++ resources/views/targets/index.blade.php
@@ -4,7 +4,56 @@
             Calcul métabolisme de base (MB) et dépense énergétique journalière (DEJ)
         </h2>
     </x-slot>
+    
+    @if(count($targets) > 0)
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-lg font-medium text-gray-900">
+                    <div class="p-6 px-0 text-lg font-medium text-gray-900">
+                        {{ __("Objectif(s) en cours") }}
+                    </div>
+                    <!-- <div class="card">
+                        <div class="card-body bg-light"> -->
+                        <table id="mesure__table" class="table table-light authortable table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>Date de début</th>
+                                        <th>Date de fin</th>
+                                        <th>Poids (Kg)</th>
+                                        <th>Modifier</th>
+                                        <th>Supprimer</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach($targets as $target)
+                                    <tr>
+                                        <td>{{ date('d/m/Y', strtotime($target->startDate)) }}</td>
+                                        <td>{{ date('d/m/Y', strtotime($target->endDate)) }}</td>
+                                        <td>{{ $target->weight }}</td>
+                                        <td> 
+                                            <a class="navbar-brand" href="{{ route('targets.edit', $target->id) }}">
+                                                <i class="fa-solid fa-pen-to-square fa-lg"></i>
+                                            </a>
+                                        </td>
+                                        <td>
+                                            <a class="navbar-brand" href="">
+                                                <i class="fa-regular fa-trash-can fa-lg"></i>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                <tbody>
+                            </table>
+                        <!-- </div>
+                    </div> -->
+                </div>
+            </div>
+        </div>
+    </div>
+    @endif
 
+    @if($mesures !== null)
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
@@ -36,7 +85,23 @@
             </div>
         </div>
     </div>
-
+    @else 
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <div class="card">
+                        <div class="card-body bg-light">
+                            <div class="mb-3 emptyMesures">
+                                <h4>Résumé indisponible, aucune mesure n'a été enregistrée</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endif
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg"> <!-- bg-white overflow-hidden shadow-sm sm:rounded-lg -->
